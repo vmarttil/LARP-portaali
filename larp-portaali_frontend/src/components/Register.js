8e2affee17b4db44b619f3a359f1ae8d7db35b0e
Ville Marttila <ville.marttila@iki.fi>
2021-02-04 21:44:52 +0200

    Converted rest of elements in registration page to Bootstrap ones

--- larp-portaali_frontend/src/components/Register.js
+++ larp-portaali_frontend/src/components/Register.js
@@ -1,50 +1,49 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { isEmail, isPassword } from "../utils/validate"
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Card, Form, Button, Alert } from 'react-bootstrap';
 import AuthService from "../services/auth.service";
 
 const Register = (props) => {
-  const firstRender = useRef(true);
 
-  const [email, setEmail] = useState({value: "", error: null});
-  const [password, setPassword] = useState({value: "", error: null});
+  const [email, setEmail] = useState({ value: "", error: null });
+  const [password, setPassword] = useState({ value: "", error: null });
 
   const [successful, setSuccessful] = useState(false);
   const [message, setMessage] = useState("");
-  
+
   const [login, setLogin] = useState(false);
 
-// onChange handlers
+  // Form field handlers
 
   const onChangeEmail = (e) => {
     const email_field = e.target.value;
-    setEmail({...email, value: email_field});
+    setEmail({ ...email, value: email_field });
   };
 
   const onChangePassword = (e) => {
     const password_field = e.target.value;
-    setPassword({...password, value: password_field});
+    setPassword({ ...password, value: password_field });
   };
 
-// Validators
+  // Validators
 
   const validateEmail = () => {
     if (email.value === "") {
-      setEmail({...email, error: "Sähköpostiosoite on pakollinen."});
+      setEmail({ ...email, error: "Sähköpostiosoite on pakollinen." });
     } else if (!isEmail(email.value)) {
-      setEmail({...email, error: "Syötä kelvollinen sähköpostiosoite."});
+      setEmail({ ...email, error: "Syötä kelvollinen sähköpostiosoite." });
     } else {
-      setEmail({...email, error: null});
+      setEmail({ ...email, error: null });
     }
   };
 
   const validatePassword = () => {
     if (password.value === "") {
-      setPassword({...password, error: "Salasana on pakollinen."});
+      setPassword({ ...password, error: "Salasana on pakollinen." });
     } else if (!isPassword(password.value)) {
-      setPassword({...password, error: "Salasanan on oltava 8-40 merkkiä pitkä."});
+      setPassword({ ...password, error: "Salasanan on oltava 8-40 merkkiä pitkä." });
     } else {
-      setPassword({...password, error: null})
+      setPassword({ ...password, error: null })
     }
   };
 
@@ -61,7 +60,7 @@ const Register = (props) => {
         setMessage(response.data.message);
         setSuccessful(true);
         setTimeout(() => setLogin(true), 2000)
-      } catch(error) {
+      } catch (error) {
         const resMessage =
           (error.response &&
             error.response.data &&
@@ -75,32 +74,19 @@ const Register = (props) => {
     }
   };
 
-  // useEffect(() => {
-  //   if (firstRender.current) {
-  //     firstRender.current = false
-  //     return
-  //   }
-  //   setButtonDisabled( !validatePassword() || !validateEmail() )
-  // }, [email.value, password.value])
-
   return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
+      <Card style={{ width: "24rem" }}>
+        <Card.Img variant="top" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="profile-img-card" />
         <Form className="align-items-center" onSubmit={handleRegister}>
           {!successful && (
             <>
               <Form.Group controlId="email_field">
                 <Form.Label>Sähköpostiosoite</Form.Label>
-                <Form.Control 
-                  type="email" 
+                <Form.Control
+                  type="email"
                   value={email.value}
                   onChange={onChangeEmail}
-                  onBlur={validateEmail}/>
+                  onBlur={validateEmail} />
                 {email.error && (
                   <Form.Text className="text-muted red">{email.error}</Form.Text>
                 )}
@@ -108,11 +94,11 @@ const Register = (props) => {
 
               <Form.Group controlId="password_field">
                 <Form.Label>Salasana</Form.Label>
-                <Form.Control 
-                  type="password" 
+                <Form.Control
+                  type="password"
                   value={password.value}
                   onChange={onChangePassword}
-                  onBlur={validatePassword}/>
+                  onBlur={validatePassword} />
                 {password.error && (
                   <Form.Text className="text-muted red">{password.error}</Form.Text>
                 )}
@@ -122,7 +108,7 @@ const Register = (props) => {
               </Form.Group>
             </>
           )}
-          <Alert show={message} variant={ successful ? "success" : "danger"}>
+          <Alert show={message} variant={successful ? "success" : "danger"}>
             {message}
           </Alert>
 
@@ -135,8 +121,7 @@ const Register = (props) => {
             )
           )}
         </Form>
-      </div>
-    </div>
+      </Card>
   );
 };
 
